20373e4de7915b6b2a99ab75b8c0f2ddd932298f
P3rtang <tomverhulst21@gmail.com>
2023-01-27 23:18:28 +0100

    added test for dialogs

--- src/tests/app.rs
+++ src/tests/app.rs
@@ -17,4 +17,15 @@ mod tests {
         app.reset_mode();
         assert_eq!(app.get_mode(), AppMode::from_bits(0b0000_0000_0001).unwrap());
     }
+    #[test]
+    fn test_dialogs() {
+        let mut app = App::default();
+        app.open_dialog(Dialog::AddNew).unwrap();
+        assert_eq!(app.get_opened_dialog(), &Dialog::AddNew);
+        assert!(app.open_dialog(Dialog::Delete).is_err());
+        app.close_dialog();
+        assert_eq!(app.get_opened_dialog(), &Dialog::None);
+        app.close_dialog();
+        assert_eq!(app.get_opened_dialog(), &Dialog::None);
+    }
 }
